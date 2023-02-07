6e28e5c256aca1c81598a698a4fcbc1ff6b64d96
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-02-06 21:36:52 -0500

    Init the Intron-Matcher repo

---
+++ build/CMakeFiles/intron_matcher.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/GArgs.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/GArgs.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/GArgs.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/GBase.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/GBase.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/GBase.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/GFaSeqGet.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/GFaSeqGet.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/GFaSeqGet.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/GStr.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/GStr.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/GStr.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/codons.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/codons.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/codons.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/gdna.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/gdna.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/gdna.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/include/gclib/gff.cpp" "CMakeFiles/intron_matcher.dir/include/gclib/gff.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/include/gclib/gff.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/src/GSam.cpp" "CMakeFiles/intron_matcher.dir/src/GSam.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/src/GSam.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/src/intron_matcher.cpp" "CMakeFiles/intron_matcher.dir/src/intron_matcher.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/src/intron_matcher.cpp.o.d"
+  "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_intron_matcher/src/tmerge.cpp" "CMakeFiles/intron_matcher.dir/src/tmerge.cpp.o" "gcc" "CMakeFiles/intron_matcher.dir/src/tmerge.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/intron_matcher.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intron_matcher.dir/include/gclib/GArgs.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GArgs.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GBase.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GBase.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GFaSeqGet.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GFaSeqGet.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GStr.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/GStr.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/include/gclib/codons.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/codons.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/include/gclib/gdna.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/gdna.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/include/gclib/gff.cpp.o"
+  "CMakeFiles/intron_matcher.dir/include/gclib/gff.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/src/GSam.cpp.o"
+  "CMakeFiles/intron_matcher.dir/src/GSam.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/src/intron_matcher.cpp.o"
+  "CMakeFiles/intron_matcher.dir/src/intron_matcher.cpp.o.d"
+  "CMakeFiles/intron_matcher.dir/src/tmerge.cpp.o"
+  "CMakeFiles/intron_matcher.dir/src/tmerge.cpp.o.d"
+  "intron_matcher"
+  "intron_matcher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intron_matcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
